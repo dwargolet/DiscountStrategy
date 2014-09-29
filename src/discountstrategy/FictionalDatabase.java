a607e39d530930380882daa90c237769a33e7f28
Daniel <Daniel@192.168.0.11>
2014-09-28 21:04:56 -0500

    FictionalDatabase should be done, may add method for adding things to database later

--- src/discountstrategy/FictionalDatabase.java
+++ src/discountstrategy/FictionalDatabase.java
@@ -25,6 +25,20 @@ public class FictionalDatabase {
     
     
     
+    private ProductInformation[] products = {
+        new ProductInformation ("1A1", "T-shirt", 1, 4.99, new FlatRateDiscount()),
+        new ProductInformation ("2B2", "Skirt", 3, 9.99, new EmployeeDiscount())    
+    };
+    
+    ProductInformation product;
+    
+      public void findProduct(String productID){
+        for(ProductInformation p : products){
+            if(productID.equals(product.getProductID())){
+                product = p;
+            }
+        }
+    }
     
     
 }
--- src/discountstrategy/StartUp.java
+++ src/discountstrategy/StartUp.java
@@ -9,8 +9,11 @@ public class StartUp {
     public static void main(String[] args) {
         
         
+        FictionalDatabase fd = new FictionalDatabase();
         
         
+        
+        fd.findCustomer("Dr4g0n");
 //        DiscountStrategy a = new EmployeeDiscount();
 //        ProductInformation ball = new ProductInformation("ABCD", "A Ball", 3, 4.33, a);
         
